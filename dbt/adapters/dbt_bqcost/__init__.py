8ad8563701bfb42f2c94049619f7632c17a08602
Ching Yi, Chan <qrtt1@infuseai.io>
2023-06-21 11:36:53 +0800

    wip
    Signed-off-by: Ching Yi, Chan <qrtt1@infuseai.io>

--- dbt/adapters/dbt_bqcost/__init__.py
+++ dbt/adapters/dbt_bqcost/__init__.py
@@ -1,13 +1,29 @@
-from dbt.adapters.dbt_bqcost.connections import dbt_bqcostConnectionManager # noqa
-from dbt.adapters.dbt_bqcost.connections import dbt_bqcostCredentials
-from dbt.adapters.dbt_bqcost.impl import dbt_bqcostAdapter
-
 from dbt.adapters.base import AdapterPlugin
-from dbt.include import dbt_bqcost
+from dbt.adapters.bigquery import BigQueryCredentials
+from dbt.adapters.factory import FACTORY
+
+from dbt.adapters.dbt_bqcost.connections import (
+    dbt_bqcostConnectionManager,
+    dbt_bqcostCredentials,
+)  # noqa
+from dbt.adapters.dbt_bqcost.impl import dbt_bqcostAdapter
+from dbt.include import bigquery
 
 
-Plugin = AdapterPlugin(
+class HackedAdapterPlugin(AdapterPlugin):
+    def __init__(self, adapter, credentials, include_path: str, dependencies=None):
+        super().__init__(adapter, credentials, include_path, dependencies)
+
+    def __getattribute__(self, item):
+        if item == "credentials":
+            if "bigquery" not in FACTORY.plugins:
+                FACTORY.load_plugin("bigquery")
+            FACTORY.plugins["bigquerycost"] = FACTORY.plugins["bigquery"]
+        return super().__getattribute__(item)
+
+
+Plugin = HackedAdapterPlugin(
     adapter=dbt_bqcostAdapter,
-    credentials=dbt_bqcostCredentials,
-    include_path=dbt_bqcost.PACKAGE_PATH
-    )
+    credentials=BigQueryCredentials,
+    include_path=bigquery.PACKAGE_PATH,
+)
