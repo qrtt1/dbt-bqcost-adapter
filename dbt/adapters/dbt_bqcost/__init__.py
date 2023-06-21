0bdfd5f38da21a6fe9cb5d1493740585494319ac
Ching Yi, Chan <qrtt1@infuseai.io>
2023-06-21 19:44:33 +0800

    wip
    Signed-off-by: Ching Yi, Chan <qrtt1@infuseai.io>

--- dbt/adapters/dbt_bqcost/__init__.py
+++ dbt/adapters/dbt_bqcost/__init__.py
@@ -1,5 +1,12 @@
+import dbt.flags
+from typing import Tuple
+
+import json
+
+import agate
 from dbt.adapters.base import AdapterPlugin
-from dbt.adapters.bigquery import BigQueryCredentials
+from dbt.adapters.bigquery import BigQueryConnectionManager, BigQueryCredentials
+from dbt.adapters.bigquery.connections import BigQueryAdapterResponse
 from dbt.adapters.factory import FACTORY
 
 from dbt.adapters.dbt_bqcost.connections import (
@@ -18,9 +25,46 @@ class HackedAdapterPlugin(AdapterPlugin):
         if item == "credentials":
             if "bigquery" not in FACTORY.plugins:
                 FACTORY.load_plugin("bigquery")
-            FACTORY.plugins["bigquerycost"] = FACTORY.plugins["bigquery"]
+            FACTORY.plugins["dbt_bqcost"] = FACTORY.plugins["bigquery"]
         return super().__getattribute__(item)
 
+
+class Patched:
+    def to_dict(self, response: BigQueryAdapterResponse) -> dict:
+        return {
+            "dbt_project_name": self.profile.project_name,
+            "bytes_processed": response.bytes_processed,
+            "bytes_billed": response.bytes_billed,
+            "location": response.location,
+            "project_id": response.project_id,
+            "job_id": response.job_id,
+            "slot_ms": response.slot_ms,
+            "message": response._message,
+        }
+
+    def execute(
+        self, sql, auto_begin=False, fetch=None
+    ) -> Tuple[BigQueryAdapterResponse, agate.Table]:
+        response, table = self.origin_execute(sql, auto_begin, fetch)
+
+        if not hasattr(self, "usages"):
+            self.usages = []
+
+            def dump_at_exit():
+                print(json.dumps(self.usages))
+                pass
+
+            import atexit
+            atexit.register(dump_at_exit)
+
+        self.usages.append(self.to_dict(response))
+
+        return response, table
+
+
+BigQueryConnectionManager.origin_execute = BigQueryConnectionManager.execute
+BigQueryConnectionManager.execute = Patched.execute
+BigQueryConnectionManager.to_dict = Patched.to_dict
 
 Plugin = HackedAdapterPlugin(
     adapter=dbt_bqcostAdapter,
